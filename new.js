5a02dde7a07d3298bfbc254355885aec463980e5
MLDODSON <mldodson@fullsail.edu>
2018-04-04 23:46:11 -0500

    Learning ES6 ways to write variables, concatenation.

---
+++ new.js
@@ -0,0 +1,156 @@
+// Declaring Variables and Constants
+
+
+//Declaring Variables. There are 3 wats to declare variables in JS and doing an alert.
+
+/*
+//Doing an alert
+alert("Welcome to learning JS with Saab!");
+
+//Standard JS
+var drink = "apple juice";
+console.log(drink);
+
+//ES6 is not supported in some older browsers
+let myFood = "pasta";
+console.log(myFood);
+
+//Using const to declare a variable means it can't be reassigned
+const myStreet = "Grand";
+console.log(myStreet);
+
+//Math can even be done
+let number = 3 * 6;
+console.log(number);
+
+var subtraction = 46 - 8;
+console.log(subtraction);
+
+const add = 76 + 14;
+console.log(add);
+
+//Using string concatenation in js
+console.log("I love drinking " + drink + " with my " + myFood + " on " + myStreet + ".");
+
+//This is a more modern way of doing concatenation using ES6.
+let restaurant = "Burger King";
+console.log(`I could use a combo meal right now from ${restaurant}!`);
+
+let name = "Chuck Wagner";
+
+const age = 46;
+
+var profession = "web developer";
+
+console.log(`${name} the ${profession} just turned ${age}.`);
+*/
+
+
+
+//Assignment: Use concatenation 5 times with each three ways to right variables
+let name = "Boba Fett";
+let series = "Star Wars";
+console.log(`${name} from ${series} is a bad dude!`);
+
+let person = "Han Solo";
+let career = "bounty hunter";
+console.log(`The ${career} ${name} is always hunting ${person}.`);
+
+let number = 12 - 6;
+let food = "eggs";
+console.log(`If I have a half dozen ${food}, then I have only ${number} left.`);
+
+let woman = "Gayle";
+let weight = 20;
+console.log(`${woman} is ${weight} pounds overweight.`);
+
+console.log(`${woman} is a huge ${series} fan!`);
+
+var place = "Puerto Plata";
+var emotion = "love";
+console.log("I just " + emotion + " " + place + "!");
+
+var group = "Funkadelic";
+var music = "funk and rock";
+console.log(group + " used to play some great " + music + " music back in the day!");
+
+var animal = "dogs";
+var animalTwo = "Cats";
+console.log(animalTwo + " are cool but " + animal + " are the best!");
+
+var hero = "The Hulk";
+var villian = "Thanos";
+console.log("Unfortunately " + hero + " can not beat " + villian + " alone.");
+
+var movie = "Avengers: Infinity War";
+var type = "fantasy";
+console.log("The " + type + " super hero movie " + movie + ", is most likely going to be the highest grossing movie of 2018.");
+
+
+const planet = "Jupiter";
+const object = "moons";
+console.log(`The planet ${planet} officially has 27 ${object}.`);
+
+const musicOne = "Michael Jackson";
+const musicTwo = "Prince";
+console.log(`When I was growing up ${musicTwo} and ${musicOne} were my favorite recording artists!`);
+
+const fruit = "Grapes";
+const veggie = "rice";
+console.log(`${fruit} are my favorite fruit and ${veggie} is my favorite veggie.`);
+
+const player = "Steph Curry";
+const team = "The Golden State Warriors";
+console.log(`${player} of ${team} is my favorite NBA player.`);
+
+const lady = "Glenny";
+const staus = "married";
+const guy = "Mike";
+console.log(`My friend ${lady} is ${staus} to a nice man named ${guy}.`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
