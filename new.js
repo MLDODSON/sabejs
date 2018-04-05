1b1fd89143d8763bca5ff7833d4aa9d057c19cac
MLDODSON <mldodson@fullsail.edu>
2018-04-05 16:00:21 -0500

    practice with ES6 and quizzes

--- new.js
+++ new.js
@@ -46,7 +46,7 @@ console.log(`${name} the ${profession} just turned ${age}.`);
 */
 
 
-
+/*
 //Assignment: Use concatenation 5 times with each three ways to right variables
 let name = "Boba Fett";
 let series = "Star Wars";
@@ -107,6 +107,127 @@ const lady = "Glenny";
 const staus = "married";
 const guy = "Mike";
 console.log(`My friend ${lady} is ${staus} to a nice man named ${guy}.`);
+*/
+
+//Using toLowerCase
+var place = "Saint Louis";
+console.log(place.toLowerCase());
+
+let boxer = "Roy Jones"
+console.log(boxer.toLowerCase() + " was a great fighter!");
+
+//Assignment. Do toLowerCase 8 times.
+
+var city = "New York";
+console.log(city.toLowerCase());
+
+var team = "New York Mets";
+console.log(team.toLowerCase());
+
+console.log(city.toLowerCase() + " is the home of the baseball " + team.toLowerCase() + ".");
+
+let season1 = "Fall";
+console.log(season1.toLowerCase());
+
+let season2 = "Spring";
+console.log(season2.toLowerCase());
+
+console.log(season2.toLowerCase() + " and " + season1.toLowerCase() + " are my favorite times of the year.");
+
+var girl = "Paola";
+var guy = "Julio";
+console.log(guy.toLowerCase() + " and " + girl.toLowerCase() + " are in love!");
+
+const person = "Pops";
+const food1 = "Chicken";
+const food2 = "Pig Feet";
+console.log(person.toLowerCase() + " says he likes " + food1.toLowerCase() + " and " + food2.toLowerCase() + ".");
+
+//Using toUpperCase. Assignment: Do toUppercase 5 times
+var pet = "Benji";
+var place = "mama's house";
+console.log(pet.toUpperCase() + " realy stinks up my " + place.toUpperCase() + ".");
+
+let town1 = "Puerto Plata";
+let town2 = "St. Louis";
+console.log("Right now " + town1.toUpperCase() + " is nice and sunny. On the other hand " + town2.toUpperCase() + " is cold and rainy today.");
+
+const dessert1 = "German Chocolate Cake";
+const dessert2 = "Cherry Pie";
+const dessert3 = "Ice Cream";
+console.log("Mama said that we can either have " + dessert1.toUpperCase() + " or " + dessert2.toUpperCase() + " with " + dessert3.toUpperCase() + " for dessert.");
+
+var movie1 = "Blade";
+var movie2 = "Avengers 1";
+var movie3 = "Spider-Man 2";
+movie1 = "Blade 2";
+console.log("Some of my favorite hero movies are " + movie1.toUpperCase() + "," + movie2.toUpperCase() + ", and " + movie3.toUpperCase() + ".");
+
+let team1 = "Cardinals";
+let team2 = "Rams";
+team2 = "Golden State Warriors";
+console.log("I enjoy watching the " + team1.toUpperCase() + " and the " + team2.toUpperCase() + " play.");
+
+
+//Assignment: Do 5 toLower and Upper combined
+var man = "Joe";
+var size = "Peon";
+console.log("That " + man.toUpperCase() + " really thinks he is important. In reality he is a " + size.toLowerCase() + ".");
+
+let flavor1 = "Chocolate Shake";
+let flaver2 = "Vanilla Shake";
+flaver2 = "Strawberry Shake";
+console.log("Right now I am in the mood for a " + flavor1.toUpperCase() + " but I will also take a " + flaver2.toLowerCase() + ".");
+
+const villian = "Thanos of Titan";
+const villian2 = "Emperor Palpatine";
+console.log("Who is a more rotten individual? " + villian2.toLowerCase() + " or " + villian.toUpperCase() + "?");
+
+var girl1 = "Sue";
+var girl2 = "Sue's";
+var item = "Socks";
+console.log("Who sews " + girl2.toUpperCase() + " " + item.toLowerCase() + "? " + girl1.toUpperCase() + " sews " + girl2.toUpperCase() + " " + item.toLowerCase() + ".");
+
+let thing = "Spaceship";
+let planet = "Mars";
+console.log("If I had a " + thing.toLowerCase() + " I would fly to " + planet.toUpperCase() + ".");
+
+const eat = "chicken";
+const eat2 = "pork and beans";
+console.log("Tonight I am in the mood for fried " + eat.toUpperCase() + " and " + eat2.toLowerCase() + ".");
+
+
+//Reviewing what you have learned
+
+//1. Write out a variable using var let and const
+var day =  "Thursday";
+console.log("Today is " + day + ".");
+
+let boy = "Chris";
+console.log(`Everybody hates ${boy}!`);
+
+const youngLady = "Paola";
+console.log(`${youngLady} is studying to become a doctor someday.`);
+
+//Concatenate all variables into one sentence.
+console.log(`${boy} and ${youngLady} are going out ${day} for pizza.`);
+
+let something = "big dreams";
+
+var view = "The Black Panther";
+view = "The Infinity War";
+
+//Use toLowerCase
+console.log(boy.toLowerCase() + " just turned 18.");
+
+//Use toUpperCase
+console.log("That " + youngLady.toLocaleUpperCase() + " has " + something.toUpperCase() + " of being rich someday.");
+
+//Use upper and lower. Also change a variable.
+console.log(youngLady.toUpperCase() + " and " + boy.toLowerCase() + " are going to see " + view.toUpperCase() + " at the movies next " + day + ".");
+
+
+
 
 
 
